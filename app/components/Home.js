e048412f7864a30e327cf4a8f8ed55c1a1e0e5f9
Jingbo Zhang <jb.zhang1234@gmail.com>
2015-12-18 16:05:01 -0500

    Add react front end code

---
+++ app/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component{
+	render:function(){
+		return (
+			<div className="alter alert-info">
+				Hello from Home Component
+			</div>
+		);
+	}
+}
+
+
+
